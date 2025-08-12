78e06eb6b3f89792683832f983714f7043e3cc78
Philippe Nunes <philippe.silva@mprj.mp.br>
2025-08-12 16:05:46 -0300

    feat: script para estração de tweets

---
+++ src/extracao_api_twitter.py
@@ -0,0 +1,31 @@
+from datetime import datetime, timedelta
+import requests
+import json
+
+TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S.00Z"
+
+end_time = datetime.now().strftime(TIMESTAMP_FORMAT)
+start_time = (datetime.now() + timedelta(-1)).date().strftime(TIMESTAMP_FORMAT)
+query = "data science"
+
+tweet_fields = "tweet.fields=author_id,conversation_id,created_at,id,in_reply_to_user_id,public_metrics,lang,text"
+user_fields = "expansions=author_id&user.fields=id,name,username,created_at"
+
+
+# url_raw = f"https://api.twitter.com/2/tweets/search/recent?query={query}&start_time={start_time}&end_time={end_time}&{tweet_fields}&{user_fields}"
+
+url_raw = f"https://labdados.com/2/tweets/search/recent?query={query}&{tweet_fields}&{user_fields}&start_time={start_time}&end_time={end_time}"
+
+response = requests.get(url_raw)
+
+json_response = response.json()
+print(json.dumps(json_response, indent=4, sort_keys=True))    
+
+while "next_token" in json_response.get("meta", {}):
+    next_token = json_response["meta"]["next_token"]
+    url = f"{url_raw}&next_token={next_token}"
+    response = requests.get(url)
+    json_response = response.json()
+    print(json.dumps(json_response, indent=4, sort_keys=True))    
+
+
