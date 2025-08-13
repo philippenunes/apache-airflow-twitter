1c7c86b8d91ec158657310e5f20be13783ec70e2
Philippe Nunes <philippe.silva@mprj.mp.br>
2025-08-13 00:58:49 -0300

    refactor: atualiza sintaxe do Airflow e implementa intervalos dinâmicos
    - Remove uso de days_ago (deprecated) e atualiza para datetime
    - Atualiza schedule_interval para schedule (nova sintaxe)
    - Implementa data_interval_start/end para timestamps dinâmicos
    - Melhora configuração de agendamento da DAG

--- airflow_pipeline/dags/twitter_dag.py
+++ airflow_pipeline/dags/twitter_dag.py
@@ -7,12 +7,13 @@ from operators.twitter_operator import TwitterOperator
 from os.path import join
 
 
-with DAG(dag_id="TwitterTest", start_date=datetime.now()) as dag:
+with DAG(dag_id="TwitterDAG", start_date=datetime.now() - timedelta(days=2), schedule="@daily") as dag:
     TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S.00Z"
-    end_time = datetime.now().strftime(TIMESTAMP_FORMAT)
-    start_time = (datetime.now() + timedelta(-1)).date().strftime(TIMESTAMP_FORMAT)
     query = "data science"
     to = TwitterOperator(file_path=join("datalake/twitter_datascience",
                                         "extract_date={{ ds }}",
                                         "datascience_{{ ds_nodash }}.json"),
-                                        query=query, start_time=start_time, end_time=end_time, task_id="test_run")
+                                        query=query,
+                                        start_time="{{ data_interval_start.strftime('%Y-%m-%dT%H:%M:%S.00Z') }}",
+                                        end_time="{{ data_interval_end.strftime('%Y-%m-%dT%H:%M:%S.00Z') }}",
+                                        task_id="twitter_datascience")
